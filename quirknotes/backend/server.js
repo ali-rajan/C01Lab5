ce8c4d71d974376bf2840ff243ff3581d800d94e
ali-rajan <ali.rajan@mail.utoronto.ca>
2024-02-15 00:05:28 -0500

    Tests added

--- quirknotes/backend/server.js
+++ quirknotes/backend/server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
-import cors from "cors";
+import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import cors from 'cors';
 
 const app = express();
 const PORT = 4000;
-const mongoURL = "mongodb://127.0.0.1:27017";
-const dbName = "quirknotes";
+const mongoURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'quirknotes';
 
 // Connect to MongoDB
 let db;
@@ -15,11 +15,11 @@ async function connectToMongo() {
 
   try {
     await client.connect();
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
 
     db = client.db(dbName);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
   }
 }
 
@@ -27,31 +27,30 @@ connectToMongo();
 
 // Open Port
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 app.use(cors());
 
 // Collections to manage
 const COLLECTIONS = {
-    notes: "notes",
-  };
-
+  notes: 'notes',
+};
 
 // Get all notes available
-app.get("/getAllNotes", express.json(), async (req, res) => {
+app.get('/getAllNotes', express.json(), async (req, res) => {
   try {
     // Find notes with username attached to them
     const collection = db.collection(COLLECTIONS.notes);
     const data = await collection.find().toArray();
-    res.json({ response: [] });
+    res.json({ response: data });
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
-})
-  
+});
+
 // Post a note
-app.post("/postNote", express.json(), async (req, res) => {
+app.post('/postNote', express.json(), async (req, res) => {
   try {
     // Basic body request check
     const { title, content } = req.body;
@@ -60,7 +59,7 @@ app.post("/postNote", express.json(), async (req, res) => {
     if (!title || !content) {
       return res
         .status(400)
-        .json({ error: "Title and content are both required." });
+        .json({ error: 'Title and content are both required.' });
     }
 
     // Send note to database
@@ -68,25 +67,24 @@ app.post("/postNote", express.json(), async (req, res) => {
     const result = await collection.insertOne({
       title,
       content,
-      createdAt
+      createdAt,
     });
     res.json({
-      response: "Note added succesfully.",
+      response: 'Note added succesfully.',
       insertedId: result.insertedId,
     });
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 // Delete a note
-app.delete("/deleteNote/:noteId", express.json(), async (req, res) => {
+app.delete('/deleteNote/:noteId', express.json(), async (req, res) => {
   try {
     // Basic param checking
     const noteId = req.params.noteId;
     if (!ObjectId.isValid(noteId)) {
-      return res.status(400).json({ error: "Invalid note ID." });
+      return res.status(400).json({ error: 'Invalid note ID.' });
     }
 
     // Find note with given ID
@@ -95,25 +93,24 @@ app.delete("/deleteNote/:noteId", express.json(), async (req, res) => {
       _id: new ObjectId(noteId),
     });
 
-
     if (data.deletedCount === 0) {
       return res
         .status(404)
-        .json({ error: "Unable to find note with given ID." });
+        .json({ error: 'Unable to find note with given ID.' });
     }
     res.json({ response: `Document with ID ${noteId} deleted.` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-})
-  
+});
+
 // Patch a note
-app.patch("/patchNote/:noteId", express.json(), async (req, res) => {
+app.patch('/patchNote/:noteId', express.json(), async (req, res) => {
   try {
     // Basic param checking
     const noteId = req.params.noteId;
     if (!ObjectId.isValid(noteId)) {
-      return res.status(400).json({ error: "Invalid note ID." });
+      return res.status(400).json({ error: 'Invalid note ID.' });
     }
 
     // Basic body request check
@@ -121,33 +118,35 @@ app.patch("/patchNote/:noteId", express.json(), async (req, res) => {
     if (!title && !content) {
       return res
         .status(400)
-        .json({ error: "Must have at least one of title or content." });
+        .json({ error: 'Must have at least one of title or content.' });
     }
 
-    
     // Find note with given ID
     const collection = db.collection(COLLECTIONS.notes);
-    const data = await collection.updateOne({
-      _id: new ObjectId(noteId),
-    }, {
-      $set: {
-        ...(title && {title}),
-        ...(content && {content})
+    const data = await collection.updateOne(
+      {
+        _id: new ObjectId(noteId),
+      },
+      {
+        $set: {
+          ...(title && { title }),
+          ...(content && { content }),
+        },
       }
-    });
+    );
 
     if (data.matchedCount === 0) {
       return res
         .status(404)
-        .json({ error: "Unable to find note with given ID." });
+        .json({ error: 'Unable to find note with given ID.' });
     }
     res.json({ response: `Document with ID ${noteId} patched.` });
   } catch (error) {
-    res.status(500).json({error: error.message})
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
-app.delete("/deleteAllNotes", express.json(), async (req, res) => {
+app.delete('/deleteAllNotes', express.json(), async (req, res) => {
   try {
     const collection = db.collection(COLLECTIONS.notes);
     const data = await collection.deleteMany({});
@@ -156,22 +155,24 @@ app.delete("/deleteAllNotes", express.json(), async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-})
-
+});
 
 app.patch('/updateNoteColor/:noteId', express.json(), async (req, res) => {
   const { noteId } = req.params;
   const { color } = req.body;
 
   if (!ObjectId.isValid(noteId)) {
-      return res.status(400).json({ error: "Invalid note ID." });
+    return res.status(400).json({ error: 'Invalid note ID.' });
   }
 
   try {
-      const collection = db.collection('notes');
-      await collection.updateOne({ _id: new ObjectId(noteId) }, { $set: { color } });
-      res.json({ message: 'Note color updated successfully.' });
+    const collection = db.collection('notes');
+    await collection.updateOne(
+      { _id: new ObjectId(noteId) },
+      { $set: { color } }
+    );
+    res.json({ message: 'Note color updated successfully.' });
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 });
--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -5,6 +5,48 @@ test('1+2=3, empty array is empty', () => {
   expect([].length).toBe(0);
 });
 
+const postNote = async (title, content) => {
+  const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: title,
+      content: content,
+    }),
+  });
+
+  expect(postNoteRes.status).toBe(200);
+  const postNoteBody = await postNoteRes.json();
+  return postNoteBody.insertedId;
+};
+
+const clearNotes = async () => {
+  const deleteAllNotesRes = await fetch(`${SERVER_URL}/deleteAllNotes`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  expect(deleteAllNotesRes.status).toBe(200);
+
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const getAllNotesBody = await getAllNotesRes.json();
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(0);
+};
+
+beforeEach(clearNotes);
+afterEach(clearNotes);
+
 test('/postNote - Post a note', async () => {
   const title = 'NoteTitleTest';
   const content = 'NoteTitleContent';
@@ -21,52 +63,182 @@ test('/postNote - Post a note', async () => {
   });
 
   const postNoteBody = await postNoteRes.json();
-
   expect(postNoteRes.status).toBe(200);
   expect(postNoteBody.response).toBe('Note added succesfully.');
 });
 
 test('/getAllNotes - Return list of zero notes for getAllNotes', async () => {
-  // Code here
-  expect(false).toBe(true);
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const getAllNotesBody = await getAllNotesRes.json();
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(0);
 });
 
 test('/getAllNotes - Return list of two notes for getAllNotes', async () => {
-  // Code here
-  expect(false).toBe(true);
+  await postNote('New note', 'Note content');
+  await postNote('Another note', 'More content');
+
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const getAllNotesBody = await getAllNotesRes.json();
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(2);
 });
 
 test('/deleteNote - Delete a note', async () => {
-  // Code here
-  expect(false).toBe(true);
+  const noteId = await postNote('New note', 'Note content');
+
+  const deleteNoteRes = await fetch(`${SERVER_URL}/deleteNote/${noteId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const deleteNoteBody = await deleteNoteRes.json();
+  expect(deleteNoteRes.status).toBe(200);
+  expect(deleteNoteBody.response).toBe(`Document with ID ${noteId} deleted.`);
 });
 
 test('/patchNote - Patch with content and title', async () => {
-  // Code here
-  expect(false).toBe(true);
+  const noteId = await postNote('A note', 'Some content');
+
+  const patchNoteRes = await fetch(`${SERVER_URL}/patchNote/${noteId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: 'New title',
+      content: 'New content',
+    }),
+  });
+
+  const patchNoteBody = await patchNoteRes.json();
+  expect(patchNoteRes.status).toBe(200);
+  expect(patchNoteBody.response).toBe(`Document with ID ${noteId} patched.`);
 });
 
 test('/patchNote - Patch with just title', async () => {
-  // Code here
-  expect(false).toBe(true);
+  const noteId = await postNote('A note', 'Some content');
+
+  const patchNoteRes = await fetch(`${SERVER_URL}/patchNote/${noteId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: 'New title',
+    }),
+  });
+
+  const patchNoteBody = await patchNoteRes.json();
+  expect(patchNoteRes.status).toBe(200);
+  expect(patchNoteBody.response).toBe(`Document with ID ${noteId} patched.`);
 });
 
 test('/patchNote - Patch with just content', async () => {
-  // Code here
-  expect(false).toBe(true);
+  const noteId = await postNote('A note', 'Some content');
+
+  const patchNoteRes = await fetch(`${SERVER_URL}/patchNote/${noteId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: 'New content',
+    }),
+  });
+
+  const patchNoteBody = await patchNoteRes.json();
+  expect(patchNoteRes.status).toBe(200);
+  expect(patchNoteBody.response).toBe(`Document with ID ${noteId} patched.`);
 });
 
 test('/deleteAllNotes - Delete one note', async () => {
-  // Code here
-  expect(false).toBe(true);
+  await postNote('A note', 'Some content');
+
+  const deleteAllNotesRes = await fetch(`${SERVER_URL}/deleteAllNotes`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const deleteAllNotesBody = await deleteAllNotesRes.json();
+  expect(deleteAllNotesRes.status).toBe(200);
+  expect(deleteAllNotesBody.response).toBe('1 note(s) deleted.');
+
+  // Verify that no notes remain
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const getAllNotesBody = await getAllNotesRes.json();
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(0);
 });
 
 test('/deleteAllNotes - Delete three notes', async () => {
-  // Code here
-  expect(false).toBe(true);
+  await postNote('A note', 'Some content');
+  await postNote('Another note', 'More content');
+  await postNote('A third note', 'Some more content');
+
+  const deleteAllNotesRes = await fetch(`${SERVER_URL}/deleteAllNotes`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const deleteAllNotesBody = await deleteAllNotesRes.json();
+  expect(deleteAllNotesRes.status).toBe(200);
+  expect(deleteAllNotesBody.response).toBe('3 note(s) deleted.');
+
+  // Verify that no notes remain
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const getAllNotesBody = await getAllNotesRes.json();
+  expect(getAllNotesRes.status).toBe(200);
+  expect(getAllNotesBody.response.length).toBe(0);
 });
 
 test('/updateNoteColor - Update color of a note to red (#FF0000)', async () => {
-  // Code here
-  expect(false).toBe(true);
+  const noteId = await postNote('A note', 'Some content');
+
+  const updateNoteColorRes = await fetch(
+    `${SERVER_URL}/updateNoteColor/${noteId}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        color: '#FF0000',
+      }),
+    }
+  );
+
+  const updateNoteColorBody = await updateNoteColorRes.json();
+  expect(updateNoteColorRes.status).toBe(200);
+  expect(updateNoteColorBody.message).toBe('Note color updated successfully.');
 });
